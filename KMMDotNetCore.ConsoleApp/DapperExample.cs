92c7f4123752f2c2d031ed35785edd9cec4a0f40
kyimoemyint <kyimoemyintkm@gmail.com>
2025-01-29 20:40:42 +0630

    feat: Dapper read create edit

---
+++ KMMDotNetCore.ConsoleApp/DapperExample.cs
@@ -0,0 +1,96 @@
+﻿using Dapper;
+using KMMDotNetCore.ConsoleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMMDotNetCore.ConsoleApp
+{
+    public class DapperExample
+    {
+        private readonly string _connectionString = "Data Source=.;Initial Catalog=DotNetTrainingBatch5;User ID=sa;Password=kmm@123";
+
+        public void Read()
+        {
+            //using(IDbConnection db = new SqlConnection(_connectionString))
+            //{
+            //    string query = "select * from tbl_blog where DeleteFlag = 0;";
+            //    var lst = db.Query(query).ToList();
+            //    foreach (var item in lst)
+            //    {
+            //        Console.WriteLine(item.BlogId);
+            //        Console.WriteLine(item.BlogTitle);
+            //        Console.WriteLine(item.BlogAuthor);
+            //        Console.WriteLine(item.BlogContent);
+            //    }
+            //}
+
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                string query = "select * from tbl_blog where DeleteFlag = 0;";
+                var lst = db.Query<BlogDataModel>(query).ToList();
+                foreach (var item in lst)
+                {
+                    Console.WriteLine(item.BlogId);
+                    Console.WriteLine(item.BlogTitle);
+                    Console.WriteLine(item.BlogAuthor);
+                    Console.WriteLine(item.BlogContent);
+                }
+            }
+        }
+
+        public void Create(string title, string author, string content)
+        {
+            string query = $@"INSERT INTO [dbo].[Tbl_Blog]
+                       ([BlogTitle]
+                       ,[BlogAuthor]
+                       ,[BlogContent]
+                       ,[DeleteFlag])
+                 VALUES
+                       (@BlogTitle
+                       ,@BlogAuthor
+                       ,@BlogContent
+                       ,0)";
+
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                int result = db.Execute(query, new BlogDataModel
+                {
+                    BlogTitle = title,
+                    BlogAuthor = author,
+                    BlogContent = content,
+                });
+                Console.WriteLine(result == 1 ? "Saving Successful." : "Saving Failed.");
+            }
+        }
+
+        public void Edit(int id)
+        {
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                string query = "select * from tbl_blog where DeleteFlag = 0 and BlogId = @BlogId;";
+                var item = db.Query<BlogDataModel>(query, new BlogDataModel
+                {
+                    BlogId = id
+                }).FirstOrDefault();
+
+                //if (item == null)
+                if (item is null)
+                {
+                    Console.WriteLine("No Data Found.");
+                    return;
+                }
+
+                Console.WriteLine(item.BlogId);
+                Console.WriteLine(item.BlogTitle);
+                Console.WriteLine(item.BlogAuthor);
+                Console.WriteLine(item.BlogContent);
+
+            }
+        }
+    }
+}
--- KMMDotNetCore.ConsoleApp/Program.cs
+++ KMMDotNetCore.ConsoleApp/Program.cs
@@ -11,11 +11,17 @@ Console.WriteLine("Hello, World!");
 //Dapper / new version of  ADO.NET (ORM = object relational mapping)
 //EFCore / entity framework (ORM)
 
-AdoDotNet adoDotNet = new AdoDotNet();
+//AdoDotNet adoDotNet = new AdoDotNet();
 //adoDotNet.Read();
 //adoDotNet.Create();
 //adoDotNet.Edit();
 //adoDotNet.Update();
-adoDotNet.Delete();
+//adoDotNet.Delete();
+
+DapperExample dapperExample = new DapperExample();
+//dapperExample.Read();
+//dapperExample.Create("asdfasdf", "asdfasdfief", "aljljhopjlj");
+//dapperExample.Edit(1);
+//dapperExample.Edit(5);
 
 Console.ReadLine();
